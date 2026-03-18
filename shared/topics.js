e67d64dcc76d1c1a925ae8bf83afb9f109d287e6
kevorkian <kevorkianmano124@gmail.com>
2026-03-18 14:49:15 +0200

    first commit

---
+++ shared/topics.js
@@ -0,0 +1,8 @@
+module.exports = {
+  USER_PREF_UPDATED: "UserPreferencesUpdated",
+  AVAILABILITY_UPDATED: "AvailabilityUpdated",
+  MATCH_FOUND: "MatchFound",
+  SESSION_CREATED: "StudySessionCreated",
+  SESSION_JOINED: "StudySessionJoined",
+  BUDDY_REQUEST: "BuddyRequestCreated",
+};
